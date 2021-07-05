9656fa578bbc67c8165189685082883703bcd72a
donellgill <donell.gill@outlook.com>
2021-07-04 20:09:49 -0400

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(deli)
+        katz_deli = []
+    if deli.empty?
+        puts "The line is currently empty."
+        line(katz_deli)
+    else
+        current_line = "The line is currently:"
+        deli.each.with_index(1) do |person, i|
+          current_line << " #{i}. #{person}"
+        end
+        puts current_line
+
+end
+
+def take_a_number(deli, name)
+        
+    take_a_number(deli, name)
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+    if deli.empty?
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{deli.first}."
+      deli.shift
+    end
+  end
